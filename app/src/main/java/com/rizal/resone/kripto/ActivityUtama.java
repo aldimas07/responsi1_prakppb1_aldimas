9cce477693b1d9b4b0ae57c46b267d3169fb0a8f
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2022-05-24 10:53:49 +0700

    nambah fungsi pindah activity

--- app/src/main/java/com/rizal/resone/kripto/ActivityUtama.java
+++ app/src/main/java/com/rizal/resone/kripto/ActivityUtama.java
@@ -2,13 +2,40 @@ package com.rizal.resone.kripto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityUtama extends AppCompatActivity {
 
+    private Button btnList, btnProfil, btnExit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_utama);
+        btnList = (Button) findViewById(R.id.btnList);
+        btnProfil = (Button) findViewById(R.id.btnProfil);
+        btnExit = (Button) findViewById(R.id.btnExit);
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityUtama.this, ListCoinActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finishAffinity();
+            }
+        });
+        btnProfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityUtama.this, ProfilActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
