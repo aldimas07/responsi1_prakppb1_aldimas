e6efc813c205b2c8f57b18d87a4f6b5610b6da04
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2022-05-24 10:33:32 +0700

    persiapan layout

---
+++ app/src/main/java/com/rizal/resone/kripto/ItemCoinActivity.java
@@ -0,0 +1,14 @@
+package com.rizal.resone.kripto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemCoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_coin);
+    }
+}
