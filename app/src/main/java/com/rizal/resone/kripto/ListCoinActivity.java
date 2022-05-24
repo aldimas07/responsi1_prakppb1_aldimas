1ae83c210fbd10d1c44721f634fc5e595f26e2bc
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2022-05-24 11:15:19 +0700

    nambah implementasi recycler view koin

--- app/src/main/java/com/rizal/resone/kripto/ListCoinActivity.java
+++ app/src/main/java/com/rizal/resone/kripto/ListCoinActivity.java
@@ -1,14 +1,27 @@
 package com.rizal.resone.kripto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.rizal.resone.kripto.adapter.KoinAdapter;
+import com.rizal.resone.kripto.model.Koin;
+
+import java.util.List;
+
 public class ListCoinActivity extends AppCompatActivity {
+
+    private RecyclerView recyKoin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_coin);
+        recyKoin = (RecyclerView) findViewById(R.id.recyKoin);
+        List<Koin> listKoin = Koin.listKoin;
+        recyKoin.setLayoutManager(new LinearLayoutManager(ListCoinActivity.this));
+        recyKoin.setAdapter(new KoinAdapter(listKoin));
     }
 }
