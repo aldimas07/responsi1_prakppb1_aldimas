5132cda8095f7f84bdafcda44ad4269ba90f8e25
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2022-05-24 11:09:17 +0700

    nambah placeholder list koin

--- app/src/main/java/com/rizal/resone/kripto/model/Koin.java
+++ app/src/main/java/com/rizal/resone/kripto/model/Koin.java
@@ -1,10 +1,18 @@
 package com.rizal.resone.kripto.model;
 
+import com.rizal.resone.kripto.R;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Koin {
     private int icon;
     private String nama;
     private String deskripsi;
     private String sejarah;
+    public static List<Koin> listKoin = Arrays.asList(
+            new Koin(R.drawable.bitcoin, "Bitcoin", "adasdasdasdasdasd", "asdasdasdasdasdasd")
+    );
 
     public Koin(int icon, String nama, String deskripsi, String sejarah) {
         this.icon = icon;
