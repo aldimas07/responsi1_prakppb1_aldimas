bebbd78127a24c178d8fd0bf0126927507f00e93
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2022-05-28 17:19:56 +0700

    nambah direct link

--- app/src/main/java/com/rizal/resone/kripto/ItemCoinActivity.java
+++ app/src/main/java/com/rizal/resone/kripto/ItemCoinActivity.java
@@ -3,7 +3,10 @@ package com.rizal.resone.kripto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,7 +14,7 @@ import com.rizal.resone.kripto.model.Koin;
 
 public class ItemCoinActivity extends AppCompatActivity {
 
-    private TextView txtNamaKoin, txtDeskripsiKoin, txtSejarahKoin;
+    private TextView txtNamaKoin, txtDeskripsiKoin, txtSejarahKoin, txtDirectLinkKoin;
     private ImageView imgGambarKoin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,7 @@ public class ItemCoinActivity extends AppCompatActivity {
         txtNamaKoin = (TextView) findViewById(R.id.txtNamaKoin);
         txtDeskripsiKoin = (TextView) findViewById(R.id.txtDeskripsiKoin);
         txtSejarahKoin = (TextView) findViewById(R.id.txtSejarahKoin);
+        txtDirectLinkKoin = (TextView) findViewById(R.id.txtDirectLinkKoin);
         imgGambarKoin = (ImageView) findViewById(R.id.imgGambarKoin);
         Intent intent = getIntent();
         Koin koin = intent.getParcelableExtra("KOIN");
@@ -27,5 +31,14 @@ public class ItemCoinActivity extends AppCompatActivity {
         txtSejarahKoin.setText(koin.getSejarah());
         txtDeskripsiKoin.setText(koin.getDeskripsi());
         imgGambarKoin.setImageResource(koin.getIcon());
+        txtDirectLinkKoin.setPaintFlags(txtDirectLinkKoin.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        txtDirectLinkKoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri webpage = Uri.parse(koin.getLink());
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
+            }
+        });
     }
 }
